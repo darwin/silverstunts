68467944c7e6bbc2157f83c8631cd9a0a5bcabe5
Antonin Hildebrand <antonin@hildebrand.cz>
2007-12-29 20:14:58 +0000

    moved content files into site
    git-svn-id: svn://localhost/ssdev/trunk@7 acbdafa1-b604-4940-a7e0-f183e313aebb

--- site/Page.py
+++ site/Page.py
@@ -1,14 +1,9 @@
 # this is python code driving our silverlight control
-# it acts as a bootstrapper (I was unable to intialize python scripting engine from managed code)
-# here I load managed assembly and route all relevant actions to it
-#
-# TODO: investigate this:
-# http://www.voidspace.org.uk/ironpython/silverlight/embedding_ironpython.shtml
-# it looks like a better solution
+# it acts as a scripting DLR bootstrapper (I was unable to intialize python scripting engine from managed code)
+# we load managed assembly and route all relevant actions to it
 
 import sys, clr
 
-# SilverStunts.dll and Physics.dll must be located in www root !?
 SilverStunts = clr.LoadAssemblyByName("SilverStunts, Version=1.0.0.0")
 
 ####################################################################
